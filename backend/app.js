28f186c5995aa39e77a257a96c7906782b3be242
Phillip Vocke <philvocke@gmail.com>
2024-10-13 00:05:18 -0400

    V9

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "angular", "browser", "index.html"));
+  res.sendFile(path.join(__dirname, "angular", "server", "index.server.html"));
 });
 
 app.post("/api/tools", (req, res, next) => {
