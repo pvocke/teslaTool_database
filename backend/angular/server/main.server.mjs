92fb595f4182500b7838141483647822c16e08e6
Phillip Vocke <philvocke@gmail.com>
2024-10-12 23:56:53 -0400

    V8

--- backend/angular/server/main.server.mjs
+++ backend/angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WXFDIIPJ.mjs";import"./chunk-HJD3H7CE.mjs";import"./chunk-LDHEPOWF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2JHWV6ZX.mjs";import"./chunk-HJD3H7CE.mjs";import"./chunk-LDHEPOWF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    production: false,
     apiURL: "http://localhost:3000/api"
 };
